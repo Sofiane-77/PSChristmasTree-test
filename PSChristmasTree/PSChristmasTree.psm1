789aa601c7cd2a4b0432c4c2be931d00ccdc7171
unknown <19627401+Sofiane-77@users.noreply.github.com>
2022-03-30 04:09:50 +0200

    :tada: Initial version

---
+++ PSChristmasTree/PSChristmasTree.psm1
@@ -0,0 +1,23 @@
+Set-StrictMode -Version Latest
+
+#Get public and private function definition files.
+    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+    foreach ($import in @($Public + $Private))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+## Export all of the public functions making them available to the user
+foreach ($file in $Public) {
+    Export-ModuleMember -Function $file.BaseName
+}
